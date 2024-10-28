cd33fdb438591dcb03fbe8ec60c8e8183fe1820c
SrijanKumar30 <srijankumar30@gmail.com>
2024-10-28 09:27:20 +0530

    Update app.py
    Update "/Summarize"

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def Index():
     return render_template("index.html")
-@app.route("/Summarize ",methods=["GET","POST"])
+@app.route("/Summarize",methods=["GET","POST"])
 
 def Summarize():
     if req.method == "POST":
